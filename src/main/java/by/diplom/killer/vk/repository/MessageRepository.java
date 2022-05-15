d8b51add883ceab8145ed1562b62e672f5ce1199
pavelUdaloi <paveludaloi@gmail.com>
2022-05-16 01:12:08 +0300

    useless commit with shit

--- src/main/java/by/diplom/killer/vk/repository/MessageRepository.java
+++ src/main/java/by/diplom/killer/vk/repository/MessageRepository.java
@@ -5,15 +5,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
-//    @Query(value = "SELECT FROM messages m WHERE m.chat_id=:chatId ORDER BY m.publication_date DESC",
-//            nativeQuery = true)
-//    Page<Message> findAllLastByChatId(Long chatId, Pageable pageable);
-
-    @EntityGraph(value = "Message.user")
+    @Query(value = "SELECT FROM messages m WHERE m.chat_id=:chatId ORDER BY m.publication_date DESC",
+            nativeQuery = true)
     Page<Message> findAllByChatIdAndOrderByPublicationDateDesc(Long chatId, Pageable pageable);
+    // todo походу это неправильное решение, нет времени править
+//    @EntityGraph(value = "Message.user")
+//    Page<Message> findAllByChatIdAndOrderByPublicationDateDesc(Long chatId, Pageable pageable);
 }
